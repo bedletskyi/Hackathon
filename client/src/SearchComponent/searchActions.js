88a88fd5c547ac35f8c384e96c36d9779297a5bd
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-01-24 22:33:35 +0200

    Configure bag and requests

--- client/src/SearchComponent/searchActions.js
+++ client/src/SearchComponent/searchActions.js
@@ -1,10 +1,10 @@
-import { setProducts } from '../ProductCardsWrapper/productCardActions';
+import { setProducts, setProductsToShow } from '../ProductCardsWrapper/productCardActions';
 
 export const SEARCH_ACTION = 'SEARCH_ACTION';
 
 export function searchAction(query) {
     return (dispatch) => {
-        const url = new URL('http://localhost:5000/price');
+        const url = new URL('http://localhost:5000/products');
         const params = { search: query };
         url.search = new URLSearchParams(params).toString();
 
@@ -13,6 +13,7 @@ export function searchAction(query) {
                 const { value } = await response.json();
 
                 dispatch(setProducts(value));
+                dispatch(setProductsToShow(value));
             } else {
                 debugger;
             }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const cors=require('cors');
-const bodyParser=require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { parserService } = require('./services/parserService');
 
 const port = 5000;
 
@@ -9,11 +10,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/price', (req, res) => {
-    const searchQuery = req.query.search || 'fisting';
-  res.send({value:`${searchQuery} is 300$`})
-})
+app.get('/products', (req, res, next) => {
+    const searchQuery = req.query.search || 'гречка';
+    parserService
+        .getDataFromSites(searchQuery)
+        .then((products) => {
+            console.log(products)
+            res.send({ value: products });
+        })
+        .catch((err) => {
+            console.log(err);
+            next(err);
+        });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
