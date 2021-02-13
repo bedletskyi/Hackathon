eb38de4bf8d40e29ba6da4cc2c74da2b8a73edcb
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2021-02-13 17:54:40 +0200

    add aws conf

--- server/develop/index.js
+++ server/develop/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { parserService } = require('./services/parserService');
 
-const port = 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
