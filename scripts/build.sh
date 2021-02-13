c2840522a842e87c71b5ec182d271871a7f27a82
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2021-02-13 16:31:30 +0200

    fix paths to files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f ../.docker/client.Dockerfile -t client ../client
-docker build -f ../.docker/server.Dockerfile -t api ../server
+docker build -f .docker/client.Dockerfile -t client client
+docker build -f .docker/server.Dockerfile -t api server
